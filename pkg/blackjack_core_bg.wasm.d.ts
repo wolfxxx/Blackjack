344208af5a70eb8bfd4a29cbc31330e0a9c2fabc
wolfxxx <pebsnel@gmail.com>
2025-11-27 09:56:13 +0100

    Add WASM build artifacts (pkg directory) for GitHub Pages

---
+++ pkg/blackjack_core_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const play_single_game: (a: any) => [number, number, number];
+export const run_simulation: (a: any) => [number, number, number];
+export const run_simulation_with_progress: (a: any, b: any) => [number, number, number];
+export const run_spot_check: (a: any) => [number, number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
